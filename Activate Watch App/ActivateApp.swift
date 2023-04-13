f2a7ebe7eb1c983eed92761f240dc1c8408c5bc2
designerjeans <jnationdesignerjeans@gmail.com>
2023-04-13 10:02:33 -0500

    a commit

---
+++ Activate Watch App/ActivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivateApp.swift
+//  Activate Watch App
+//
+//  Created by Jaden Nation on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Activate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Activate/ActivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivateApp.swift
+//  Activate
+//
+//  Created by Jaden Nation on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ActivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
